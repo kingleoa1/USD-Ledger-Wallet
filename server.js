c061076cc2a5fabdca6da8f2df93cad0514e55d1
Leonardo Smith <kingleothird@gmail.com>
2025-04-05 20:34:44 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+// server.js
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql2/promise');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const http = require('http');
+const { initWebSocket } = require('./utils/websocket');
+
+const userRoutes = require('./routes/userRoutes');
+const transactionRoutes = require('./routes/transactionRoutes');
+const cardRoutes = require('./routes/cardRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+
+const app = express();
+const server = http.createServer(app);
+
+// Middleware
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/transactions', transactionRoutes);
+app.use('/api/cards', cardRoutes);
+app.use('/api/admin', adminRoutes);
+
+// Default Route
+app.get('/', (req, res) => res.send('Virtual USD Wallet API running'));
+
+// Init WebSocket Server
+initWebSocket(server);
+
+// Launch Server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
